1348fb6b5065dae85ec09feb8063c9660de7196e
Harsh Barnawa <harshbarnawa.info@gmail.com>
2026-06-28 17:01:41 +0530

    Finally backend connected to frontend //now adding login cred

--- apps/dashboard/app/auth/login/page.tsx
+++ apps/dashboard/app/auth/login/page.tsx
@@ -1,7 +1,51 @@
+"use client";
+
+import { useState } from "react";
+import api from "@/lib/api";
+
 export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleLogin() {
+    try {
+      const res = await api.post("/auth/login", {
+        email,
+        password,
+      });
+
+      alert("Login Success");
+
+      console.log(res.data);
+    } catch (err) {
+      alert("Login Failed");
+      console.error(err);
+    }
+  }
+
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Login Page</h1>
+    <main className="min-h-screen flex flex-col items-center justify-center gap-4">
+      <h1 className="text-3xl font-bold">Login</h1>
+
+      <input
+        className="border p-2 rounded w-80"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        className="border p-2 rounded w-80"
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button
+        className="bg-black text-white px-5 py-2 rounded"
+        onClick={handleLogin}
+      >
+        Login
+      </button>
     </main>
   );
 }
--- services/gateway/main.py
+++ services/gateway/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from starlette.middleware.sessions import SessionMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 load_dotenv()
@@ -10,6 +11,16 @@ from services.gateway.app.api.v1.api import router
 app = FastAPI(
     title="Mintok Gateway",
     version="1.0.0"
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 app.add_middleware(
     SessionMiddleware,
